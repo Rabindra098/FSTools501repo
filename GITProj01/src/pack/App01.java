60a62ff14071e9b8d31183b9e3e85e41ae42510b
Rabindra098 <101rabindrakumar@gmail.com>
2025-08-09 18:59:18 +0530

    commit

---
+++ GITProj01/src/pack/App01.java
@@ -0,0 +1,10 @@
+package pack;
+
+public class App01 {
+	public static void main(String[] args) {
+		App02 a1 = new App02();
+		a1.meth1();
+		a1.name("Raju");
+	}
+
+}
---
+++ GITProj01/src/pack/App02.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class App02 {
+	void meth1() {
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+	}
+	public String name(String name) {
+		System.out.println("Hello");
+		return name;
+	}
+}
